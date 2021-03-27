8e5da141f5bbab9e9edfb057920a84462d0339a1
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-03-27 19:46:38 -0300

    Criado README.md

---
+++ flask/poke_app/README.md
@@ -0,0 +1,8 @@
+# MyPykedex
+<image src="https://cdn.bulbagarden.net/upload/9/95/Dream_Master_Ball_Sprite.png"
+                    width=50
+                    height=50
+/> <image   src="https://www.python.org/static/community_logos/python-powered-w-100x40.png"
+            width=130
+            height=50
+            />
--- flask/poke_app/dict_to_object.py
+++ flask/poke_app/dict_to_object.py
@@ -20,18 +20,20 @@ my_dict={
                     'bb':{ 'aba':1, 'abb':2, 'abc':3 },
                     'bc':{ 'bca':1, 'bcb':2, 'bcc':3 }   
                 },
-            'c':[{  'c0a': {'c0aa':1, 'c0ab':2, 'c0ac':3},
-                    'c0b': {'c0ba':1, 'c0bb':2, 'c0bc':3},
-                    'c0c': {'c0ca':1, 'c0cb':2, 'c0cc':3},
-                },
-                {   'c1a': {'c1aa':1, 'c1ab':2, 'c1ac':3},
-                    'c1b': {'c1ba':1, 'c1bb':2, 'c1bc':3},
-                    'c1c': {'c1ca':1, 'c1cb':2, 'c1cc':3},
-                },
-                {   'c2a': {'c2aa':1, 'c2ab':2, 'c2ac':3},
-                    'c2b': {'c2ba':1, 'c2bb':2, 'c2bc':3},
-                    'c2c': {'c2ca':1, 'c2cb':2, 'c2cc':3},
-                }],       
+            'c':[
+                    {  'c0a': {'c0aa':1, 'c0ab':2, 'c0ac':3},
+                        'c0b': {'c0ba':1, 'c0bb':2, 'c0bc':3},
+                        'c0c': {'c0ca':1, 'c0cb':2, 'c0cc':3},
+                    },
+                    {   'c1a': {'c1aa':1, 'c1ab':2, 'c1ac':3},
+                        'c1b': {'c1ba':1, 'c1bb':2, 'c1bc':3},
+                        'c1c': {'c1ca':1, 'c1cb':2, 'c1cc':3},
+                    },
+                    {   'c2a': {'c2aa':1, 'c2ab':2, 'c2ac':3},
+                        'c2b': {'c2ba':1, 'c2bb':2, 'c2bc':3},
+                        'c2c': {'c2ca':1, 'c2cb':2, 'c2cc':3},
+                    }
+                ],       
         }
 # namedtuple transforma um dicionário em um objeto com a classe definida
 # porém não transforma listas ou objectos aninhados.
